667a96d51c9fde8e1d942faa852e451d5bc2192d
404-nut-pound <souls4523@gmail.com>
2023-04-06 00:54:08 +0900

    [feat] 컴포넌트 바인딩

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
+  import Movie from './movie.svelte'
+
   let name = 'World'
+  let movieList = ['A', 'BB', 'CCC', 'DDDD']
 </script>
 
 <h1>Hello {name}!</h1>
 
 <input type="text" bind:value={name} />
 <button on:click={() => (name = 'World')}>초기화</button>
+
+<Movie {movieList} />
 
 <style>
   h1 {
---
+++ src/routes/movie.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let movieList: string[] = []
+</script>
+
+<ul>
+  {#each movieList as movie}
+    <li>{movie}</li>
+  {/each}
+</ul>
