4bbe50edb624b7e17d27405637b46bb3f2809086
404-nut-pound <souls4523@gmail.com>
2023-04-06 01:58:52 +0900

    [feat] 변수 저장 기능 구현

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,13 @@
+<script lang="ts">
+  import { setContext } from 'svelte'
+  import { writable } from 'svelte/store'
+
+  const user = writable<string>()
+  $: user.set('USER NAME!')
+
+  setContext('user', user)
+</script>
+
 <a href="/actor">영화 배우</a>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
+  import { getContext } from 'svelte'
   import Movie from './movie.svelte'
 
-  let name = 'World'
+  const defaultUser = getContext('user')
+  let userName = $defaultUser
   let movieList = ['몬티 파이튼의 성배', '아이언 맨', '올드보이']
 </script>
 
-<h1>Hello {name}!</h1>
+<h1>Hello {userName}!</h1>
 
-<input type="text" bind:value={name} />
-<button on:click={() => (name = 'World')}>초기화</button>
+<input type="text" bind:value={userName} />
+<button on:click={() => (userName = $defaultUser)}>초기화</button>
 
 <Movie {movieList} />
 
